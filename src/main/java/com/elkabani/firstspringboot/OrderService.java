7875aba46a3df70b3ab0f02e6a6856d751ee6754
ielkabani <elkabaim@ucmail.uc.edu>
2025-09-11 14:29:07 -0400

    Refactor payment service injection and update service annotations for Paypal and Stripe

--- src/main/java/com/elkabani/firstspringboot/OrderService.java
+++ src/main/java/com/elkabani/firstspringboot/OrderService.java
@@ -1,12 +1,17 @@
 package com.elkabani.firstspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     private PaymentService paymentService;
-    public OrderService(PaymentService paymentService) {
+    public OrderService(){}
+
+  @Autowired
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
     public void placeOrder(){
